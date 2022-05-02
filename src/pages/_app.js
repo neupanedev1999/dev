b05e8aeec572a208971bf6c0687773fa19b28c2f
neupanedev1999 <neupanedev2055@gmail.com>
2022-05-02 15:28:13 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <title>
+    <head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8718363685404871"
+     crossorigin="anonymous"></script>
+    </head>
         Devraj Neupane| Tech Enthusiast | Information Security Researcher
       </title>
       <Theme>
