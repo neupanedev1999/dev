02c6c8b036e9ccb016b08d19d680d2b2c53d7eec
neupanedev1999 <neupanedev2055@gmail.com>
2022-05-04 22:09:20 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,13 @@ import Theme from '../styles/theme'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <title>
     <head>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8718363685404871"
      crossorigin="anonymous"></script>
-    </head>
+      <title>
         Devraj Neupane| Tech Enthusiast | Information Security Researcher
       </title>
+    </head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
