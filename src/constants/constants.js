cd0c2e6e016865a412310664a8bdeee73cdd8adb
neupanedev1999 <neupanedev2055@gmail.com>
2023-09-14 01:30:53 +0100

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,7 @@
 export const projects = [
 	{
 		title: "Sample Project 1",
-		description:
-			"Insert Project Description Here,",
+		description: "Insert Project Description Here",
 		image: "/Path/To/The/Image/1.png",
 		tags: ["Tag 1", "Tag 2", "Tag 3", "Tag 4"],
 		source: "https://example.com/source",
@@ -22,14 +21,72 @@ export const TimeLineData = [
 export const Resume =
   'https://github.com/neupanedev1999/portfolio-website/blob/master/public/resume.docx?raw=true'
 
-export const AboutMeText = 'Enthusiastic IT student eager to contribute to success through hard work, attention to detail, 
-and excellent organizational skills. A clear understanding of computers, networks, and their 
-security. motivated to learn, grow, and excel in the field of computer programming and cyber 
-security
-, '
+export const AboutMeText = `
+Devraj Neupane
+London | neupanedev2055@gmail.com | [LinkedIn](https://www.linkedin.com/in/neupanedev/) | 07459938978
+
+PROFESSIONAL SUMMARY
+
+A BSc computing systems graduate with a strong foundation in system security, data analytics, and database systems, seeking an opportunity in Cyber Security to leverage my knowledge and gain practical experience in pursuit of a career in the field. Throughout my university education, I consistently sought opportunities for growth and self-improvement, evident in my acquisition of additional certificates and training. With a high level of focus and discipline, both in my academic and professional development, I am now eager to secure a position as a Cyber Security Analyst, where I can effectively utilize my expertise and skills to contribute significantly to a team and support broader business objectives.
+
+EDUCATION
+
+2019-2023 BSc (HONS) Computing Systems with Adv. Diploma in Professional Practice, Ulster University, London Grade: 2:1 (second class upper division)
+Modules: Mathematics for Computing, System Security, Data Analytics, Database Systems, Web Application Development
+
+2016-2018 Computer Science, Tilottama College, Nepal grade: (3.3/4)
+Modules: Physics, Chemistry, Mathematics, Computer Science
+
+Languages
+- English
+- Hindi
+- Nepali
+
+RELEVANT EXPERIENCE
+
+Tryhackme Training
+- Participated in online, cloud-based, cybersecurity training platform to enhance knowledge of cyber security through short, gamified real-world lab-based training.
+- Solved 113 machines and awarded 18 badges
+
+Portswigger Academy
+- Completed Web Security Academy, featuring over 190 topics and interactive labs that cover trends in vulnerabilities, risks and threats.
+
+WORK EXPERIENCE
+
+2021-2023 Co-op food, North Greenwich, London Customer Team Member
+- Excellent communication skills developed through daily interactions with customers, conveying information, listening to concerns, and providing clear explanations.
+- Strong problem-solving abilities gained from finding creative solutions to meet customer needs and resolve conflicts.
+- Proven teamwork and collaboration skills through successful collaboration with colleagues to deliver outstanding customer service and solve problems efficiently.
+- Customer service orientation with a focus on meeting customer needs, ensuring satisfaction, and handling data with confidentiality.
+- Ability to adapt to various situations, think critically, and work under pressure, honed through customer-facing roles.
+- Attention to detail and accuracy in customer interactions.
+- Commitment to maintaining confidentiality and handling sensitive information discreetly.
+- Multitasking and prioritization skills developed in a fast-paced customer service environment, applicable to the dynamic nature of cyber security.
+
+CERTIFICATIONS
+
+TryHackMe
+- Pre-Security Learning Path
+- Complete Beginner Learning Path
+- CompTIA Pentest+ Learning Path
+- Web Fundamentals Learning Path
+
+LinkedIn Learning
+- Linux Operating System
+- Cyber Security Skills.
+
+SKILLS
+
+Network Security Tools: Nmap, Nessus, hydra, Netcat, Wireshark, Metasploit framework
+Web App Security Tools: Burp Suite, Nikto, SQLmap, FFUF, Amass, Httpx, Nuclei, Wpscan
+Intelligence Gathering Tools: Maltego, Rengine, Shodan, Google dorks
+Programming Language: Python
+
+References available on request
+`;
 
 export const FooterContent = {
-	"emailAddress":"neupanedev2055@gmail.com",
-	"phoneNumber":"+44 07459938978",
-	"footerText":"This is the footer text. Please change the information from constants.js file"
-} 
+	emailAddress: "neupanedev2055@gmail.com",
+	phoneNumber: "+44 07459938978",
+	footerText: "This is the footer text. Please change the information from constants.js file"
+};
