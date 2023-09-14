bb30d28382d46ec4f844ebe6d88c619fa49be709
neupanedev1999 <neupanedev2055@gmail.com>
2023-09-14 01:34:03 +0100

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -22,67 +22,80 @@ export const Resume =
   'https://github.com/neupanedev1999/portfolio-website/blob/master/public/resume.docx?raw=true'
 
 export const AboutMeText = `
-Devraj Neupane
-London | neupanedev2055@gmail.com | [LinkedIn](https://www.linkedin.com/in/neupanedev/) | 07459938978
+<h2>Devraj Neupane</h2>
+<p><strong>London |</strong> <a href="mailto:neupanedev2055@gmail.com">neupanedev2055@gmail.com</a> | <a href="https://www.linkedin.com/in/neupanedev/">LinkedIn</a> | 07459938978</p>
 
-PROFESSIONAL SUMMARY
+<h3>PROFESSIONAL SUMMARY</h3>
+<p>A BSc computing systems graduate with a strong foundation in system security, data analytics, and database systems, seeking an opportunity in Cyber Security to leverage my knowledge and gain practical experience in pursuit of a career in the field. Throughout my university education, I consistently sought opportunities for growth and self-improvement, evident in my acquisition of additional certificates and training. With a high level of focus and discipline, both in my academic and professional development, I am now eager to secure a position as a Cyber Security Analyst, where I can effectively utilize my expertise and skills to contribute significantly to a team and support broader business objectives.</p>
 
-A BSc computing systems graduate with a strong foundation in system security, data analytics, and database systems, seeking an opportunity in Cyber Security to leverage my knowledge and gain practical experience in pursuit of a career in the field. Throughout my university education, I consistently sought opportunities for growth and self-improvement, evident in my acquisition of additional certificates and training. With a high level of focus and discipline, both in my academic and professional development, I am now eager to secure a position as a Cyber Security Analyst, where I can effectively utilize my expertise and skills to contribute significantly to a team and support broader business objectives.
+<h3>EDUCATION</h3>
+<p><strong>2019-2023</strong> BSc (HONS) Computing Systems with Adv. Diploma in Professional Practice, Ulster University, London<br>
+Grade: 2:1 (second class upper division)<br>
+Modules: Mathematics for Computing, System Security, Data Analytics, Database Systems, Web Application Development</p>
 
-EDUCATION
+<p><strong>2016-2018</strong> Computer Science, Tilottama College, Nepal<br>
+grade: (3.3/4)<br>
+Modules: Physics, Chemistry, Mathematics, Computer Science</p>
 
-2019-2023 BSc (HONS) Computing Systems with Adv. Diploma in Professional Practice, Ulster University, London Grade: 2:1 (second class upper division)
-Modules: Mathematics for Computing, System Security, Data Analytics, Database Systems, Web Application Development
+<h4>Languages</h4>
+<ul>
+  <li>English</li>
+  <li>Hindi</li>
+  <li>Nepali</li>
+</ul>
 
-2016-2018 Computer Science, Tilottama College, Nepal grade: (3.3/4)
-Modules: Physics, Chemistry, Mathematics, Computer Science
+<h3>RELEVANT EXPERIENCE</h3>
 
-Languages
-- English
-- Hindi
-- Nepali
+<p><strong>Tryhackme Training</strong></p>
+<ul>
+  <li>Participated in online, cloud-based, cybersecurity training platform to enhance knowledge of cyber security through short, gamified real-world lab-based training.</li>
+  <li>Solved 113 machines and awarded 18 badges</li>
+</ul>
 
-RELEVANT EXPERIENCE
+<p><strong>Portswigger Academy</strong></p>
+<ul>
+  <li>Completed Web Security Academy, featuring over 190 topics and interactive labs that cover trends in vulnerabilities, risks and threats.</li>
+</ul>
 
-Tryhackme Training
-- Participated in online, cloud-based, cybersecurity training platform to enhance knowledge of cyber security through short, gamified real-world lab-based training.
-- Solved 113 machines and awarded 18 badges
+<h3>WORK EXPERIENCE</h3>
 
-Portswigger Academy
-- Completed Web Security Academy, featuring over 190 topics and interactive labs that cover trends in vulnerabilities, risks and threats.
+<p><strong>2021-2023</strong> Co-op food, North Greenwich, London</p>
+<p><em>Customer Team Member</em></p>
+<ul>
+  <li>Excellent communication skills developed through daily interactions with customers, conveying information, listening to concerns, and providing clear explanations.</li>
+  <li>Strong problem-solving abilities gained from finding creative solutions to meet customer needs and resolve conflicts.</li>
+  <li>Proven teamwork and collaboration skills through successful collaboration with colleagues to deliver outstanding customer service and solve problems efficiently.</li>
+  <li>Customer service orientation with a focus on meeting customer needs, ensuring satisfaction, and handling data with confidentiality.</li>
+  <li>Ability to adapt to various situations, think critically, and work under pressure, honed through customer-facing roles.</li>
+  <li>Attention to detail and accuracy in customer interactions.</li>
+  <li>Commitment to maintaining confidentiality and handling sensitive information discreetly.</li>
+  <li>Multitasking and prioritization skills developed in a fast-paced customer service environment, applicable to the dynamic nature of cyber security.</li>
+</ul>
 
-WORK EXPERIENCE
+<h3>CERTIFICATIONS</h3>
 
-2021-2023 Co-op food, North Greenwich, London Customer Team Member
-- Excellent communication skills developed through daily interactions with customers, conveying information, listening to concerns, and providing clear explanations.
-- Strong problem-solving abilities gained from finding creative solutions to meet customer needs and resolve conflicts.
-- Proven teamwork and collaboration skills through successful collaboration with colleagues to deliver outstanding customer service and solve problems efficiently.
-- Customer service orientation with a focus on meeting customer needs, ensuring satisfaction, and handling data with confidentiality.
-- Ability to adapt to various situations, think critically, and work under pressure, honed through customer-facing roles.
-- Attention to detail and accuracy in customer interactions.
-- Commitment to maintaining confidentiality and handling sensitive information discreetly.
-- Multitasking and prioritization skills developed in a fast-paced customer service environment, applicable to the dynamic nature of cyber security.
+<p><strong>TryHackMe</strong></p>
+<ul>
+  <li>Pre-Security Learning Path</li>
+  <li>Complete Beginner Learning Path</li>
+  <li>CompTIA Pentest+ Learning Path</li>
+  <li>Web Fundamentals Learning Path</li>
+</ul>
 
-CERTIFICATIONS
+<p><strong>LinkedIn Learning</strong></p>
+<ul>
+  <li>Linux Operating System</li>
+  <li>Cyber Security Skills</li>
+</ul>
 
-TryHackMe
-- Pre-Security Learning Path
-- Complete Beginner Learning Path
-- CompTIA Pentest+ Learning Path
-- Web Fundamentals Learning Path
+<h3>SKILLS</h3>
 
-LinkedIn Learning
-- Linux Operating System
-- Cyber Security Skills.
+<p>Network Security Tools: Nmap, Nessus, hydra, Netcat, Wireshark, Metasploit framework</p>
+<p>Web App Security Tools: Burp Suite, Nikto, SQLmap, FFUF, Amass, Httpx, Nuclei, Wpscan</p>
+<p>Intelligence Gathering Tools: Maltego, Rengine, Shodan, Google dorks</p>
+<p>Programming Language: Python</p>
 
-SKILLS
-
-Network Security Tools: Nmap, Nessus, hydra, Netcat, Wireshark, Metasploit framework
-Web App Security Tools: Burp Suite, Nikto, SQLmap, FFUF, Amass, Httpx, Nuclei, Wpscan
-Intelligence Gathering Tools: Maltego, Rengine, Shodan, Google dorks
-Programming Language: Python
-
-References available on request
+<p>References available on request</p>
 `;
 
 export const FooterContent = {
