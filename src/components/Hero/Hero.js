4da26a71d868f865aec19fc71e4b08b100ecba61
neupanedev1999 <neupanedev2055@gmail.com>
2024-07-12 10:09:38 +0100

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,20 @@
 // src/components/Hero/Hero.js
 
 import React from 'react';
+import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroStyles';
 
 const Hero = () => (
-  <section id="hero">
-    <h1>Welcome to My Portfolio</h1>
-    <p>Ulster University BSc graduate in Computing Systems, aspiring to kickstart a career in Information Security and IT. Eager for hands-on experience, I've actively pursued extra certifications and training during my academic journey.</p>
-    <a href="/path/to/resume.pdf" download>Download Resume</a>
-  </section>
+  <HeroContainer>
+    <HeroContent>
+      <HeroItems>
+        <HeroH1>Dev Neupane</HeroH1>
+        <HeroP>Ulster University BSc graduate in Computing Systems, aspiring to kickstart a career in Information Security and IT. Eager for hands-on experience, I've actively pursued extra certifications and training during my academic journey.</HeroP>
+        <HeroBtn href="/path/to/resume.pdf" download>Download Resume</HeroBtn>
+      </HeroItems>
+    </HeroContent>
+  </HeroContainer>
 );
 
 export default Hero;
+
 
