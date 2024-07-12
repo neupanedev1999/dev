e16ec781f1a5593ce6c6f89e99854f1c0a0c548a
neupanedev1999 <neupanedev2055@gmail.com>
2024-07-12 10:12:02 +0100

    Create Education.js

---
+++ src/components/Education.js
@@ -0,0 +1,17 @@
+// src/components/Education/Education.js
+
+import React from 'react';
+
+const Education = () => (
+  <section id="education">
+    <h2>Education</h2>
+    <h3>BSc (HONS) Computing Systems with Adv. Diploma in Professional Practice, Ulster University, London</h3>
+    <p>2019 – 2023</p>
+    <ul>
+      <li>Achieved a 2:1 Upper Second degree with focus on Software Development, System Security, Web Development, Data Analytics, Database System, and Artificial Intelligence.</li>
+      <li>Experienced with Python, R, Laravel, and JavaScript for university project development.</li>
+    </ul>
+  </section>
+);
+
+export default Education;
