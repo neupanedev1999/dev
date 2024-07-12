9b16f0e25685f73578452e657a2c23354760d15a
neupanedev1999 <neupanedev2055@gmail.com>
2024-07-12 10:08:38 +0100

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,39 @@
 // src/components/Header/Header.js
 
 import React from 'react';
+import { Container, Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './HeaderStyles';
 
 const Header = () => (
-  <header>
-    <h1>Dev Neupane</h1>
-    <p>London, UK | 07459938978 | <a href="mailto:Neupanedev2055@gmail.com">Neupanedev2055@gmail.com</a></p>
-    <p><a href="https://www.linkedin.com/in/neupanedev" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></p>
-    <nav>
-      <ul>
-        <li><a href="#about">About</a></li>
-        <li><a href="#education">Education</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
+  <Container>
+    <Nav>
+      <NavLink to='/'>
+        <h1>Dev Neupane</h1>
+      </NavLink>
+      <Bars />
+      <NavMenu>
+        <NavLink to='/about' activeStyle>
+          About
+        </NavLink>
+        <NavLink to='/education' activeStyle>
+          Education
+        </NavLink>
+        <NavLink to='/projects' activeStyle>
+          Projects
+        </NavLink>
+        <NavLink to='/skills' activeStyle>
+          Skills
+        </NavLink>
+        <NavLink to='/contact' activeStyle>
+          Contact
+        </NavLink>
+      </NavMenu>
+      <NavBtn>
+        <NavBtnLink to='/contact'>Contact Me</NavBtnLink>
+      </NavBtn>
+    </Nav>
+  </Container>
 );
 
 export default Header;
+
 
