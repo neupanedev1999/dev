ed2a89e157cb27b5018845d6144b2fc633308851
neupanedev1999 <neupanedev2055@gmail.com>
2024-07-12 10:03:59 +0100

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,73 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
-import {
-  AiFillGithub,
-  AiFillInstagram,
-  AiFillLinkedin,
-  AiFillMediumCircle,
-  AiFillTwitterCircle,
-} from 'react-icons/ai'
-import { DiCssdeck } from 'react-icons/di'
+// src/components/Header/Header.js
 
-import {
-  Container,
-  Div1,
-  Div2,
-  Div3,
-  NavLink,
-  SocialIcons,
-  Span,
-} from './HeaderStyles'
+import React from 'react';
 
 const Header = () => (
-  <Container>
-    <Div1>
-      <Link href="/">
-        <a
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            color: 'white',
-            marginBottom: '20px',
-          }}
-        >
-          <DiCssdeck size="3rem" />
-          <Span>Devraj Neupane</Span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href="#about">
-          <NavLink>About</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#experties">
-          <NavLink>Experties</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#showcase">
-          <NavLink>Showcase</NavLink>
-        </Link>
-      </li>
-    </Div2>
-    <Div3>
-      <SocialIcons href="https://github.com/Neupanedev1999" target="_blank">
-        <AiFillGithub size="3rem" />
-      </SocialIcons>
-      <SocialIcons href="https://medium.com/#" target="_blank">
-        <AiFillMediumCircle size="3rem" />
-      </SocialIcons>
-      <SocialIcons href="https://linkedin.com/in/neupanedev" target="_blank">
-        <AiFillLinkedin size="3rem" />
-      </SocialIcons>
-      <SocialIcons href="https://twitter.com/devrajneupane4" target="_blank">
-        <AiFillTwitterCircle size="3rem" />
-      </SocialIcons>
-    </Div3>
-  </Container>
-)
+  <header>
+    <h1>Dev Neupane</h1>
+    <p>London, UK | 07459938978 | <a href="mailto:Neupanedev2055@gmail.com">Neupanedev2055@gmail.com</a></p>
+    <p><a href="https://www.linkedin.com/in/neupanedev" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a></p>
+    <nav>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+);
 
-export default Header
+export default Header;
+
